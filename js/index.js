ae91f3420fa7838a5414423302126470db9c82e4
MamatovY <mamatovyiman28@gmail.com>
2023-08-20 12:50:05 +0600

    add scroll to top

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const navLinks = document.querySelectorAll('.nav__left a');
+const backToTopButton = document.getElementById('backToTop');
 
 navLinks.forEach(link => {
     link.addEventListener('click', smoothScroll);
@@ -16,3 +17,21 @@ function smoothScroll(e) {
         behavior: 'smooth'
     });
 }
+
+backToTopButton.style.display = 'none';
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 500) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
